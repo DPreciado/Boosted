37d33d424f3bfc3c6ad13dc9089066dca57c3b93
David <arturo_preciado@live.com.mx>
2021-05-24 13:38:57 -0700

    ajustes de conexión

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MLAPI;
+using MLAPI.Spawning;
 using MLAPI.Transports.UNET;
 using UnityEngine.UI;
 using TMPro;
@@ -10,6 +11,22 @@ public class MenuScript : MonoBehaviour
 {
     public GameObject menuPanel;
     public TMP_InputField inputField;
+
+    private void Start()
+    {
+        NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
+    }
+
+    private void ApprovalCheck(byte[] connectionData, ulong clientId, MLAPI.NetworkManager.ConnectionApprovedDelegate callback)
+    {
+        bool approve = false;
+        string password = System.Text.Encoding.ASCII.GetString(connectionData);
+        if(password == "pass")
+        {
+            approve = true;
+        }
+        callback(true, null, approve, new Vector3(2,0,0), Quaternion.identity);
+    }
 
     public void Host()
     {
@@ -22,12 +39,13 @@ public class MenuScript : MonoBehaviour
         if(inputField.text.Length <= 0)
         {
             //NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = "127.0.0.1";
-            NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = "177.227.46.140";
+            NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = "192.168.100.4";
         }
         else
         {
             NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = inputField.text;
         }
+        NetworkManager.Singleton.NetworkConfig.ConnectionData = System.Text.Encoding.ASCII.GetBytes("pass");
         NetworkManager.Singleton.StartClient();
         menuPanel.SetActive(false);
     }
